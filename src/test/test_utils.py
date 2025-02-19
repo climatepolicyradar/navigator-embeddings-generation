d9a4028aa59e71cf3d3dd89acc57ebe8181e65ff
Kalyan Dutia <kalyan.dutia@gmail.com>
2025-02-19 14:36:10 +0300

    fix failing test

--- src/test/test_utils.py
+++ src/test/test_utils.py
@@ -28,7 +28,7 @@ def test_filter_on_block_type(test_parser_output_array):
     assert filtered_inputs[0].html_data.text_blocks[0].type == "Table"
     assert filtered_inputs[0].html_data.text_blocks[0].text == ["test_text"]
 
-    assert filtered_inputs[0].html_data.text_blocks[1].type == "Google Text Block"
+    assert filtered_inputs[0].html_data.text_blocks[1].type == "sectionHeading"
     assert filtered_inputs[0].html_data.text_blocks[1].text == ["test_text"]
 
     # Assert that we can filter on IndexerInputs that don't have valid text
@@ -38,7 +38,7 @@ def test_filter_on_block_type(test_parser_output_array):
     assert filtered_inputs[1].html_data.text_blocks[0].type == "Table"
     assert filtered_inputs[1].html_data.text_blocks[0].text == ["test_text"]
 
-    assert filtered_inputs[1].html_data.text_blocks[1].type == "Google Text Block"
+    assert filtered_inputs[1].html_data.text_blocks[1].type == "sectionHeading"
     assert filtered_inputs[1].html_data.text_blocks[1].text == ["test_text"]
 
 
